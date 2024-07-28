2541a6d55f96c56bda0f09526006c8db81d2a992
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2024-07-28 16:39:35 +0900

    load image and show it on web

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import WebcamUI from './components/common/WebcamUI.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div><h1>乃木坂衣装識別</h1></div>
+  <WebcamUI />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -81,5 +71,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
