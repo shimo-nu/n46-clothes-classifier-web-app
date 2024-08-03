40efaa04733a85f83188aac2dc1fba24e76e1ebb
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2024-08-03 13:41:10 +0900

    change classes

--- src/data/yolo_classes.ts
+++ src/data/yolo_classes.ts
@@ -1,4 +1,9 @@
 export default [
-  'Aeroplane',   'Bicycle', 'Bird',  'Boat',      'Bottle', 'Bus',         'Car',   'Cat',  'Chair', 'Cow',
-  'Diningtable', 'Dog',     'Horse', 'Motorbike', 'Person', 'Pottedplant', 'Sheep', 'Sofa', 'Train', 'Tvmonitor'
+  '10_uniform', '11_uniform', '12_uniform', '13_uniform', '14_uniform', 
+  '15_uniform', '16_uniform', '17_uniform', '18_uniform', '19_uniform', 
+  '1_uniform', '20_uniform', '21_uniform', '22_uniform', '23_uniform', 
+  '24_uniform', '25_uniform', '26_uniform', '27_uniform', '28_uniform', 
+  '29_uniform', '2_uniform', '30_uniform', '31_uniform', '32_uniform', 
+  '33_uniform', '34_uniform', '35_uniform', '3_uniform', '4_uniform', 
+  '5_uniform', '6_uniform', '7_uniform', '8_uniform', '9_uniform'
 ];
