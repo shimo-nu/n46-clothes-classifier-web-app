a9e96870b44ae109d83651d3f01fb7ff39cf0cd2
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2025-06-06 10:38:17 +0900

    fix: fix some router and vite config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { authGuard } from '@auth0/auth0-vue'
 import HomeView from '../views/HomeView.vue'
 import Yolo from '../views/Yolo.vue'
 import Annotation from '../views/Annotation.vue'
 import NewImageView from '../views/NewImageView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
     {
       path: '/clothes-classification',
       name: 'clothes-classification',
       component: Yolo,
-      props: { classificationType: 'uniform' }
+      props: { classificationType: 'uniform' },
+      beforeEnter: authGuard
     },
     {
       path: '/music-costume-classification',
       name: 'music-costume-classification',
       component: Yolo,
-      props: { classificationType: 'musicCostume' }
+      props: { classificationType: 'musicCostume' },
+      beforeEnter: authGuard
     },
     {
       path: '/annotation/:handleImage/:labelCategoryName',
@@ -38,12 +39,15 @@ const router = createRouter({
       component: Annotation,
       props: route => ({ 
         handleImage: route.params.handleImage, 
-        labelCategoryName: route.params.labelCategoryName })
+        labelCategoryName: route.params.labelCategoryName 
+      }),
+      beforeEnter: authGuard
     },
     {
       path: '/new-image',
       name: 'new-image',
-      component: NewImageView
+      component: NewImageView,
+      beforeEnter: authGuard
     },
     {
       path: '/new-annotation/:handleImage/:labelCategoryName',
@@ -53,7 +57,8 @@ const router = createRouter({
         handleImage: route.params.handleImage, 
         labelCategoryName: route.params.labelCategoryName,
         isNewAnnotation: true
-      })
+      }),
+      beforeEnter: authGuard
     }
   ]
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import env from "vite-plugin-env-compatible";
+import fs from 'fs'
+import path from 'path'
+
+// SSL証明書の設定
+const httpsOptions = {
+  key: fs.readFileSync(path.resolve(__dirname, './certs/key.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, './certs/cert.pem')),
+}
+
 // import typescript from '@rollup/plugin-typescript';
 
 export default defineConfig({
@@ -14,6 +23,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
     tsconfigPaths(),
+    env({ prefix: "VITE", mountedPath: "process.env" }),
     viteStaticCopy({
       targets: [
         {
@@ -24,8 +34,13 @@ export default defineConfig({
     }),
   ],
   server: {
-    mimeTypes: {
-      'application/wasm': ['wasm']
+    host: '0.0.0.0',
+    port: 8080,
+    strictPort: true,
+    https: httpsOptions,
+    hmr: {
+      clientPort: 8080,
+      protocol: 'wss'
     }
   },
   resolve: {
